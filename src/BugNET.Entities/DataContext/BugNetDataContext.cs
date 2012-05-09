7d1cbd0ff72069d527f11d3c4a8eeab4669cb0d5
JackyW <spizs803@gmail.com>
2012-05-09 17:49:20 +0800

    add test project

---
+++ src/BugNET.Entities/DataContext/BugNetDataContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+
+namespace BugNET.Entities.DataContext
+{
+    public class BugNetDataContext : DbContext
+    { 
+        public DbSet<Project> Projects { get; set; }
+    }
+}
---
+++ src/BugNet_MVC_Tests/DBContext_Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting; 
+
+namespace BugNET.Tests.MVC_Test
+{
+    [TestClass]
+    public class DBContext_Test
+    {
+        [TestMethod]
+        public void DBContextCreated()
+        {
+            BugNET.Entities.DataContext.BugNetDataContext context = new Entities.DataContext.BugNetDataContext();
+
+            try
+            {
+                var projects = context.Projects.ToList();
+                Assert.IsNotNull(projects);
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+            
+            
+           
+        }
+    }
+}
